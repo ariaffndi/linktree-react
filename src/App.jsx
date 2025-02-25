9c6cf7b1f55bde3df0bbabb1c990c41053c8d331
ari afandi <ari.affndii.com>
2025-02-25 23:51:35 +0700

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
 		<>
 			<div className="min-h-screen flex flex-col items-center justify-center">
-				<SplashCursor />;
+				<SplashCursor />
 				<Header />
 				<MainContent />
 				<SocialMedia />
