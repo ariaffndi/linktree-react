66a542fe1303cfb136562555adedfcccf5e89fae
ari afandi <ari.affndii.com>
2025-02-25 21:49:38 +0700

    `Added SplashCursor component to App component layout`

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,21 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import SocialMedia from './components/SocialMedia'
 import Footer from './components/Footer'
+import SplashCursor from "./components/SplashCursor";
 
 function App() {
 
   return (
-    <>
-      <div className="min-h-screen flex flex-col items-center justify-center">
-        <Header />
-        <MainContent />
-        <SocialMedia />
-        <Footer />
-      </div>
-    </>
-  )
+		<>
+			<div className="min-h-screen flex flex-col items-center justify-center">
+				<SplashCursor />;
+				<Header />
+				<MainContent />
+				<SocialMedia />
+				<Footer />
+			</div>
+		</>
+	);
 }
 
 export default App
